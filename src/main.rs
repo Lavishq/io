72e5b0fa2dda49249bc38e194b2049300fb1dd58
lavishq <yourname@example.com>
2024-02-23 18:18:02 +0100

    extracting the arg parser

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("searching for {} in file {}", query, file_path);
     let content = fs::read_to_string(file_path).expect("should be able to redd");
     println!("text: \n {content}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
