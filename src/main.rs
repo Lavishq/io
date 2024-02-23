80aca7332cf7652a8c1195bff725555aad28e8e2
lavishq <yourname@example.com>
2024-02-23 17:57:52 +0100

    reading file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,4 +7,6 @@ fn main() {
     let file_path = &args[2];
 
     println!("searching for {} in file {}", query, file_path);
+    let content = fs::read_to_string(file_path).expect("should be able to redd");
+    println!("text: \n {content}");
 }
