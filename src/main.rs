ec3d03416421bedb7c3d331da8295b75f4bcaa59
lavishq <yourname@example.com>
2024-02-23 19:12:30 +0100

    seperated logic to lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,31 +14,8 @@ fn main() {
         "searching for {} in file {}",
         config.query, config.file_path
     );
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("application err: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_path)?;
-    println!("text: \n {content}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enuggh args");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
