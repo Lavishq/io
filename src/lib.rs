0ae0e8e4f585f172f783d158adf3dcfd913a29c0
lavishq <yourname@example.com>
2024-02-26 18:48:32 +0100

    add test b4 code for tdd

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("text: \n {content}");
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
